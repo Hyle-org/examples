752bba3a9afdfe04aedf4698af8138444c172f7c
maximilien-hyle <maximilien@hyle.eu>
2025-01-06 17:51:36 +0100

    ✨ Add simple identity example

---
+++ simple-identity/contract/src/lib.rs
@@ -0,0 +1,114 @@
+use std::collections::BTreeMap;
+
+use bincode::{Decode, Encode};
+use serde::{Deserialize, Serialize};
+
+use sdk::{identity_provider::IdentityVerification, Digestable};
+use sha2::{Digest, Sha256};
+
+#[derive(Encode, Decode, Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct AccountInfo {
+    pub hash: String,
+    pub nonce: u32,
+}
+
+#[derive(Encode, Decode, Serialize, Deserialize, Debug, Clone)]
+pub struct Identity {
+    identities: BTreeMap<String, AccountInfo>,
+}
+
+impl Identity {
+    pub fn new() -> Self {
+        Identity {
+            identities: BTreeMap::new(),
+        }
+    }
+
+    pub fn get_nonce(&self, username: &str) -> Result<u32, &'static str> {
+        let info = self.get_identity_info(username)?;
+        let state: AccountInfo =
+            serde_json::from_str(&info).map_err(|_| "Failed to parse accounf info")?;
+        Ok(state.nonce)
+    }
+}
+
+impl Default for Identity {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl IdentityVerification for Identity {
+    fn register_identity(
+        &mut self,
+        account: &str,
+        private_input: &str,
+    ) -> Result<(), &'static str> {
+        let id = format!("{account}:{private_input}");
+        let mut hasher = Sha256::new();
+        hasher.update(id.as_bytes());
+        let hash_bytes = hasher.finalize();
+        let account_info = AccountInfo {
+            hash: hex::encode(hash_bytes),
+            nonce: 0,
+        };
+
+        if self
+            .identities
+            .insert(account.to_string(), account_info)
+            .is_some()
+        {
+            return Err("Identity already exists");
+        }
+        Ok(())
+    }
+
+    fn verify_identity(
+        &mut self,
+        account: &str,
+        nonce: u32,
+        private_input: &str,
+    ) -> Result<bool, &'static str> {
+        match self.identities.get_mut(account) {
+            Some(stored_info) => {
+                if nonce != stored_info.nonce {
+                    return Err("Invalid nonce");
+                }
+                let id = format!("{account}:{private_input}");
+                let mut hasher = Sha256::new();
+                hasher.update(id.as_bytes());
+                let hashed = hex::encode(hasher.finalize());
+                if *stored_info.hash != hashed {
+                    return Ok(false);
+                }
+                stored_info.nonce += 1;
+                Ok(true)
+            }
+            None => Err("Identity not found"),
+        }
+    }
+
+    fn get_identity_info(&self, account: &str) -> Result<String, &'static str> {
+        match self.identities.get(account) {
+            Some(info) => Ok(serde_json::to_string(&info).map_err(|_| "Failed to serialize")?),
+            None => Err("Identity not found"),
+        }
+    }
+}
+
+impl Digestable for Identity {
+    fn as_digest(&self) -> sdk::StateDigest {
+        sdk::StateDigest(
+            bincode::encode_to_vec(self, bincode::config::standard())
+                .expect("Failed to encode Balances"),
+        )
+    }
+}
+impl From<sdk::StateDigest> for Identity {
+    fn from(state: sdk::StateDigest) -> Self {
+        let (state, _) = bincode::decode_from_slice(&state.0, bincode::config::standard())
+            .map_err(|_| "Could not decode identity state".to_string())
+            .unwrap();
+        state
+    }
+}
