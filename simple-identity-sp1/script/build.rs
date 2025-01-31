172bcd4b82eb5e287f34faad82e87d40bee8e79f
bruhhgnik <agnikmajumder.ccu@gmail.com>
2025-01-31 02:12:36 +0000

    sp1 migration

--- simple-identity-sp1/script/build.rs
+++ simple-identity-sp1/script/build.rs
@@ -1,5 +1,5 @@
 use sp1_helper::build_program_with_args;
 
 fn main() {
-    build_program_with_args("../program", Default::default())
+    build_program_with_args("../program", Default::default());
 }
