4ec46c4b2c4badb06c568923e341119e366b4c87
Lancelot de Ferrière <wraitii@gmail.com>
2025-05-23 13:13:20 +0200

    Update packaging

--- check-secret-noir/frontend/vite.config.js
+++ check-secret-noir/frontend/vite.config.js
@@ -1,6 +1,20 @@
-export default {
-  optimizeDeps: {
-    esbuildOptions: { target: "esnext" },
-    exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
-  }
-};
+import { defineConfig } from "vite";
+//import analyzer from "vite-bundle-analyzer";
+
+// https://vite.dev/config/
+export default defineConfig({
+    build: {
+        lib: {
+            entry: "lib.ts",
+            name: "check-secret",
+            fileName: (format) => `check-secret.${format}.js`,
+            formats: ["es", "cjs"],
+        },
+        rollupOptions: {
+            external: ["@aztec/bb.js", "@noir-lang/noirc_abi", "@noir-lang/acvm_js"],
+        },
+        sourcemap: true,
+        minify: true,
+    },
+    //plugins: [analyzer()],
+});
